c897450eb2154f5c461cb55b3993fbd2e9ba0b83
by-westik <78916184+by-westik@users.noreply.github.com>
2021-12-15 14:09:39 +0300

    Update ICash.h

--- lab_2/ICash.h
+++ lab_2/ICash.h
@@ -1,30 +1,24 @@
 #include <iostream>
 #include <chrono>
-
 #include "IDictionary.h"
 #include "Queue.h"
 
-template <class Key, class Element> class ICash{
+template <template <class,class> class IDictionary, class Key, class Element> class ICash{
 private:
-
-        SimpleDictionary <Key,Element> *table;
+        IDictionary <Key,Element> *table;
         Queue <Key> *queue;//time_t
         std::chrono::duration <double> maxTime = std::chrono::duration <double> (0.01);
-
 public:
-
         ICash(int _size)
-        :table(new SimpleDictionary <Key,Element> (_size))
+        :table(new IDictionary <Key,Element> (_size))
         ,queue(new Queue <Key> ())
         {};
-
+        
         ~ ICash(){
-
                 delete table;
                 delete queue;
-
         };
-
+        
         void printTable(){
                 table -> printDictionary();
         };
@@ -32,45 +26,24 @@ public:
         void printQueue(){
                 queue -> printQueue();
         };
-
+        
         void add(const Key &_key, const Element &_element){
-
                 if(this -> queue -> getHead() != NULL){
-
-//                      std::cout << "head != NULL" << std::endl;
-/*
-                        if(duration_cast<duration<double>>(high_resolution_clock::now() - (queue -> getHead() -> getTime())) >= maxTime){
-
-                                std::cout << ">>" << std::endl;
-
-                        };
-*/
-                        while((this -> queue -> getCurrentSize() > this -> table -> getMaxSize()) || (this -> queue -> getHead()) && (duration_cast<duration<double>>(h$
-//                              std::cout << "delete " << std::endl;
+                        while((this -> queue -> getCurrentSize() >= this -> table -> getMaxSize()) || (this -> queue -> getHead()) && (duration_cast<duration<double>>(high_resolution_clock::now() - (queue -> getHead() -> getTime()))) >= maxTime ){
                                 table -> remove(queue -> getHead() -> getData());
-//                              std::cout << "remove" << std::endl;
                                 queue -> pop();
-//                              std::cout << "pop" << std::endl;
-
                         };
-//                      std::cout <<"after while" << std::endl;
                 };
                 this -> queue -> push(_key);
-//              std::cout << "push"<< std::endl;
                 this -> table -> add(_key, _element);
-
+        };
+        
+        int getCurrentSize() const {
+                return this -> table -> getCount();
         };
 
-/*      int getCurrentSize() const {
-
-                return this -> table -> getCurrentSize();
-
-        };
-*/
         void remove(const Key &_key){
-
                 queue -> remove(_key);
                 table -> remove(_key);
         };
-
 };
