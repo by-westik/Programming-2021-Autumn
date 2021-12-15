2ea33b58b2dbf4ba50c04668d147fe18b22774d4
by-westik <78916184+by-westik@users.noreply.github.com>
2021-12-15 14:06:19 +0300

    Update test.cpp

--- lab_2/test.cpp
+++ lab_2/test.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,43 +6,31 @@
 
 #include "ICash.h"
 
-void generateCash(ICash <std::string,int> *cash){
-
+template <template <class,class> class IDictionary> void generateCash(ICash <IDictionary,std::string,int> *cash){
         std::ifstream myfile("animals.txt");
-        std::vector <int> randomNumbers(200);// = new std::vector <int> (200);//generateRandomNumbers(200);
+        std::vector <int> randomNumbers(200);
         for(int i = 0; i < 200; i++){
                 randomNumbers[i] = i;
-        };
-
+        }
         std::random_shuffle (randomNumbers.begin(), randomNumbers.end());
-
         if(myfile.is_open()){
-
                 std::string line;
                 int n = 0;
-
                 while(std::getline(myfile, line)){
                         cash -> add(line, randomNumbers[n]);
                         n++;
                 };
-
-//      std::cout << "n = " << n << std::endl;
-
         };
         myfile.close();
-
 };
-
 int main(){
-
-        //ICash<int,std::string> *cash = new ICash <int,std::string> (100);
-        ICash<std::string,int> *cash = new ICash <std::string,int> (100);
+        ICash<TwoHashDictionary,std::string,int> *cash = new ICash <TwoHashDictionary,std::string,int> (100);
+//      ICash<SimpleDictionary,std::string,int> *cash = new ICash <SimpleDictionary,std::string,int> (100);
         generateCash(cash);
         cash -> printQueue();
         std::cout<<std::endl<<std::endl;
         cash -> printTable();
         std::cout<<std::endl<<std::endl;
         delete cash;
-
         return 0;
 };
